570bcc8cc5412b0f7afb4ef14495db4782312a1f
ZuweiLi0 <47547270+ZuweiLi0@users.noreply.github.com>
2019-03-01 13:46:27 -0500

    Update UserOfSpeakers.java

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,6 +1,6 @@
 public class UserOfSpeakers{
     public static void main(String commandLine[]) {
         Animal x= new() Retriever;
-        
+        System.out.println (x.speak());
     }
     }
